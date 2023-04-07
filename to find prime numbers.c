e2f6f2287724b35b6a052948dccbfe314dae1d0a
Yejianx <130019293+Yejianx@users.noreply.github.com>
2023-04-08 00:12:01 +0800

    Update to find prime numbers.c

--- to find prime numbers.c
+++ to find prime numbers.c
@@ -3,43 +3,45 @@
 #include<stdio.h>
 #include<math.h>
 
-int main()
-{
-	int i = 0;//用于判断是否为素数的值
-	int j = 0;//取值范围
-	int count=0;//记录素数的个数
-    
-	for (j = 101; j <= 200; j+=2)
-	{
-		if (j % 2 == 0)//除二就等于除于所有偶数。
-		{
-		
-		}
-		else 
-		{
-			for (i = 3; i * i <= j; i +=2)//非素数必能写成两个数相乘，两个数之间相差越小，需要判断的数量也就越小，所以是开根。
-			{
-		
+#define false 0
+#define ture 1
 
+int prime( int j)
+{
+	int i = 0;//用于判断是否为素数的值/to judge whether it is a prime number
+
+		if (j% 2 == 0)//除二就等于除于所有偶数。/to divide 2 is to divide all even numbers.
+		{
+
+		}
+		else
+		{
+			for (i = 3; i * i <= j; i += 2)//非素数必能写成两个数相乘，两个数之间相差越小，需要判断的数量也就越小，所以是开根。
+			{
+				
 				if (j % i == 0)
 				{
-					break;
-			
+					return false;
+				
 				}
 			}
 		}
-		
-
-		if (i *i> j)
-		{
-			printf("%d ", j);
-			count++;
-		
-		}
-		
-	}
-  
-	printf("\n%d个 ", count);
 	
-	return 0;
+			return ture;
+}
+
+int main()
+{
+	int num = 0;
+
+	scanf("%d",&num);
+
+	if (prime(num))
+	{
+		printf("this is prime");
+	}
+	else
+	{
+		printf("this is not prime");
+	}
 }
